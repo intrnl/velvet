75476fcfdba4756a1e251a5beda4b7d9ff19bbe3
intrnl <intrnl28+git@gmail.com>
2021-12-24 22:20:19 +0700

    add some test for template transformer

---
+++ packages/compiler/test/transform_template.test.js
@@ -0,0 +1,75 @@
+import { describe, it, expect } from 'vitest';
+
+import { parse_template } from '../src/parse_template.js';
+import { transform_template } from '../src/transform_template.js';
+import { print } from '../src/utils/js_parse.js';
+
+
+describe('attribute', () => {
+	it('static', () => {
+		let template = `<div class='foo'></div>`;
+
+		let fragment = parse_template(template);
+		let program = transform_template(fragment);
+
+		expect(print(program)).toMatchSnapshot();
+	});
+
+	it('dynamic', () => {
+		let template = `<div class={className}></div>`;
+
+		let fragment = parse_template(template);
+		let program = transform_template(fragment);
+
+		expect(print(program)).toMatchSnapshot();
+	});
+
+	it('static boolean', () => {
+		let template = `<textarea readonly></textarea>`;
+
+		let fragment = parse_template(template);
+		let program = transform_template(fragment);
+
+		expect(print(program)).toMatchSnapshot();
+	});
+
+	it('dynamic boolean', () => {
+		let template = `<textarea ?readonly={is_readonly}></textarea>`;
+
+		let fragment = parse_template(template);
+		let program = transform_template(fragment);
+
+		expect(print(program)).toMatchSnapshot();
+	});
+});
+
+describe('component', () => {
+	it('v:self on a custom element', () => {
+		let template = `<x-app><v:self>hello world!</v:self></x-app>`;
+
+		let fragment = parse_template(template);
+		let program = transform_template(fragment);
+
+		expect(print(program)).toMatchSnapshot();
+	});
+});
+
+describe('expression', () => {
+	it('custom element', () => {
+		let template = `<x-app>hello {name}!</x-app>`;
+
+		let fragment = parse_template(template);
+		let program = transform_template(fragment);
+
+		expect(print(program)).toMatchSnapshot();
+	});
+
+	it('component', () => {
+		let template = `<Main>hello {name}!</Main>`;
+
+		let fragment = parse_template(template);
+		let program = transform_template(fragment);
+
+		expect(print(program)).toMatchSnapshot();
+	});
+});
