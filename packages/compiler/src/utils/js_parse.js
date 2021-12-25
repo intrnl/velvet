ab9f1ddc2e8a7b76382fdc7fec38fd4cb82df213
intrnl <intrnl28+git@gmail.com>
2021-12-25 11:36:15 +0700

    allow placeholder comments

--- packages/compiler/src/utils/js_parse.js
+++ packages/compiler/src/utils/js_parse.js
@@ -113,6 +113,14 @@ function inject_placeholders (node, values) {
 			delete node.start;
 			delete node.end;
 
+			if (node.type === 'Line' || node.type === 'Block') {
+				node.value = node.value.replace(unique_regex, (match, index) => (
+					values[+index] || match
+				));
+
+				return;
+			}
+
 			if (node.type === 'Identifier') {
 				unique_regex.lastIndex = 0;
 				let match = unique_regex.exec(node.name);
