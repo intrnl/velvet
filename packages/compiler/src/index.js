e2cf04b6caff56691f3a63fa2e1add325af14e04
intrnl <intrnl28+git@gmail.com>
2021-12-27 10:06:31 +0700

    provide compiler entrypoints

---
+++ packages/compiler/src/index.js
@@ -0,0 +1,5 @@
+export * from './compile.js';
+export * from './parse_template.js';
+export * from './transform_script.js';
+export * from './transform_template.js';
+export * from './validate_module.js';
