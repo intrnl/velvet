478c6ac8bb4cad1eb818d766b993d556882c1679
intrnl <intrnl28+git@gmail.com>
2022-01-04 21:56:34 +0700

    add more tests

--- packages/compiler/test/transform_template.test.js
+++ packages/compiler/test/transform_template.test.js
@@ -377,6 +377,34 @@ describe('conditional logic', () => {
 
 		expect(print(program)).toMatchSnapshot();
 	});
+
+	it('conditional before expression', () => {
+		let template = `
+			{#if loading}
+				Loading!
+			{/if}
+
+			<div>Hello, {name}!</div>
+		`;
+
+		let fragment = parse_template(template);
+		let program = transform_template(fragment);
+
+		expect(print(program)).toMatchSnapshot();
+	});
+
+	it('conditional containing expression', () => {
+		let template = `
+			{#if person}
+				{person.first_name} {person.last_name}
+			{/if}
+		`;
+
+		let fragment = parse_template(template);
+		let program = transform_template(fragment);
+
+		expect(print(program)).toMatchSnapshot();
+	});
 });
 
 describe('loop logic', () => {
