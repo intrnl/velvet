a5c29e00cd88fb2db2cbcc726db1f267ba8fc344
intrnl <intrnl28+git@gmail.com>
2022-01-07 08:31:42 +0700

    fix incorrect module specifier

--- packages/compiler/src/index.js
+++ packages/compiler/src/index.js
@@ -1,4 +1,4 @@
-export let COMPILER_VERSION = '0.3.10';
+export let COMPILER_VERSION = '0.3.11';
 
 export * from './compile.js';
 export * from './parse_template.js';
@@ -6,4 +6,4 @@ export * from './transform_script.js';
 export * from './transform_template.js';
 export * from './validate_module.js';
 
-export { CompilerError } from './utils/error';
+export { CompilerError } from './utils/error.js';
