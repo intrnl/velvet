b245f03a2684d4f97e70cd1b1cc439b950d207c6
intrnl <intrnl28+git@gmail.com>
2022-01-13 18:45:13 +0700

    chore: add tests for reactivity

---
+++ packages/runtime/test/internal/reactivity.test.js
@@ -0,0 +1,116 @@
+import { vi, describe, it, expect } from 'vitest';
+
+import { ref, access, effect, computed, scope } from '../../internal/reactivity.js';
+
+
+describe('ref', () => {
+	it('creates a ref', () => {
+		let value = ref(0);
+
+		expect(value(access)).toBe(0);
+
+		expect(value(2)).toBe(2);
+
+		value(4);
+		expect(value(access)).toBe(4);
+	});
+});
+
+describe('effects', () => {
+	it('creates an effect', () => {
+		let value = ref(0);
+
+		let fn = vi.fn(() => value(access));
+		let instance = effect(fn);
+
+		expect(fn).toHaveBeenCalledTimes(1);
+
+		value(2);
+		expect(fn).toHaveBeenCalledTimes(2);
+
+		value(2);
+		expect(fn).toHaveBeenCalledTimes(2);
+
+		value(4);
+		expect(fn).toHaveBeenCalledTimes(3);
+
+		instance.stop();
+
+		value(6);
+		expect(fn).toHaveBeenCalledTimes(3);
+	});
+});
+
+describe('computed', () => {
+	it('creates a computed', () => {
+		let value = ref(0);
+
+		let fn = vi.fn(() => value(access) * 2);
+		let double = computed(fn);
+
+		expect(fn).toHaveBeenCalledTimes(1);
+
+		expect(double(access)).toBe(0);
+		expect(fn).toHaveBeenCalledTimes(1);
+
+		value(2);
+		expect(double(access)).toBe(4);
+		expect(fn).toHaveBeenCalledTimes(2);
+
+		value(2);
+		expect(double(access)).toBe(4);
+		expect(fn).toHaveBeenCalledTimes(2);
+
+		value(3);
+		expect(double(access)).toBe(6);
+		expect(fn).toHaveBeenCalledTimes(3);
+	});
+});
+
+describe('scope', () => {
+	it('creates a scope', () => {
+		let instance = scope(true);
+
+		let value = ref(0);
+		let fn = vi.fn(() => value(access));
+
+		instance.run(() => {
+			effect(fn);
+		});
+
+		expect(fn).toHaveBeenCalledTimes(1);
+
+		value(2);
+		expect(fn).toHaveBeenCalledTimes(2);
+
+		instance.stop();
+
+		value(4);
+		expect(fn).toHaveBeenCalledTimes(2);
+	});
+
+	it('creates a nested scope', () => {
+		let instance = scope(true);
+
+		let value = ref(0);
+		let fn = vi.fn(() => value(access));
+
+		instance.run(() => {
+			let child = scope();
+
+			child.run(() => {
+				effect(fn);
+			});
+		});
+
+		expect(fn).toHaveBeenCalledTimes(1);
+
+		value(2);
+		expect(fn).toHaveBeenCalledTimes(2);
+
+		instance.stop();
+
+		value(4);
+		expect(fn).toHaveBeenCalledTimes(2);
+	});
+});
