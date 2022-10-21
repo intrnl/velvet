fcdfc9c0105b71d9d220b2302f54f16f12fbb761
intrnl <intrnl28+git@gmail.com>
2022-10-21 12:40:22 +0700

    chore: remove version display from playground

--- playground/src/main.js
+++ playground/src/main.js
@@ -1,8 +1,2 @@
-const runtimeVersion = document.getElementById('runtime-version');
-runtimeVersion.textContent = RUNTIME_VERSION;
-
-const compilerVersion = document.getElementById('compiler-version');
-compilerVersion.textContent = COMPILER_VERSION;
-
 const commitHash = document.getElementById('commit-hash');
 commitHash.textContent = COMMIT_HASH;
