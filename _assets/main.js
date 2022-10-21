5392294787ad149f0c76b2773e98f396b92c8770
intrnl <intrnl28+git@gmail.com>
2022-10-21 12:40:49 +0700

    deploy: f3332282df166ec76a5957358e0bd4000e6b1761

--- _assets/main.js
+++ _assets/main.js
@@ -1,2 +1,2 @@
-var t=document.getElementById("runtime-version");t.textContent="v0.4.18";var e=document.getElementById("compiler-version");e.textContent="v0.7.10";var n=document.getElementById("commit-hash");n.textContent="0379b1";
+var t=document.getElementById("commit-hash");t.textContent="f33322";
 //# sourceMappingURL=main.js.map
